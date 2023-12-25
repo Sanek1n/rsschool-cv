f1f004803aabe90ec9d7ee5f3575470a1bfdafe0
Sanek1n <sanekin8ru@outlook.com>
2023-12-25 20:37:04 +0400

    feat: add cv content

---
+++ script.js
@@ -0,0 +1,15 @@
+const openImage = document.getElementById('open-image');
+const backImage = document.getElementById("image-full");
+const closeButton = document.querySelector('.bg-close');
+
+console.log(closeButton);
+
+closeButton.addEventListener('click', () => {
+  openImage.style.display = "none";
+  openImage.removeChild(document.getElementById("image-full"));
+});
+
+function getCertificate(fileName) {
+  openImage.insertAdjacentHTML('beforeend', `<img class="back-content" id="image-full" src="assets/img/${fileName}.jpg" alt="certificate">`);
+  openImage.style.display = "block";
+}
